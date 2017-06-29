bb71b9aa6719fe01d0fc3978c972f9c22063385d
nhan <nhan@codistry.io>
2017-06-28 23:53:17 -0700

    Updated Binary Tree

--- binaryTree.js
+++ binaryTree.js
@@ -1,5 +1,7 @@
 function BinaryTree(){
 
+  this.root = null;
+
   let Node = function(key){
     this.key = key;
     this.left = null;
@@ -9,12 +11,48 @@ function BinaryTree(){
   this.insert = function(key){
     let newNode = new Node(key);
 
-    if(root == null){
-      root = newNode;
+    if(this.root == null){
+      this.root = newNode;
     } else {
-      insertNode(root, newNode);
+      insertNode(this.root, newNode);
     }
   }
 
-  let insertNode(){}
+  let insertNode = function(node, newNode){
+    if(node.key > newNode.key){
+      if(node.left == null){
+        node.left = newNode;
+      } else {
+        insertNode(node.left, newNode);
+      }
+    } else {
+      if(node.right == null){
+        node.right = newNode;
+      } else {
+        insertNode(node.right,newNode);
+      }
+    }
+    
+  }
+
+  this.preorder = function(node){
+    if(node == null){
+      return;
+    }
+
+    console.log(node.key);
+    this.preorder(node.left);
+    this.preorder(node.right);
+
+  }
 }
+
+
+let tree = new BinaryTree();
+
+tree.insert(6);
+tree.insert(2);
+tree.insert(5);
+tree.insert(10);
+tree.insert(7);
+tree.preorder(tree.root);
