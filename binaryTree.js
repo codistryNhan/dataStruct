d60eb87c526d69633d2f1b62bbfd102b6bb0c442
nhan <nhan@codistry.io>
2017-06-30 13:50:40 -0700

    Updated Binary Tree with Breadth First Traversal

--- binaryTree.js
+++ binaryTree.js
@@ -1,15 +1,22 @@
-function BinaryTree(){
+//  Binary Tree Data Stucture
 
+function BinaryTree(){
+  //Entry point to tree
   this.root = null;
 
+  //Node to hold key and link to other nodes
   let Node = function(key){
     this.key = key;
     this.left = null;
     this.right = null;
   }
 
+  //Insert method inserts a key into a node
+  //and then calls on insertNode method to insert
+  //node into tree
   this.insert = function(key){
     let newNode = new Node(key);
+    console.log("Insert: " + newNode.key);
 
     if(this.root == null){
       this.root = newNode;
@@ -18,6 +25,7 @@ function BinaryTree(){
     }
   }
 
+  //Helper method to insert nodes into tree
   let insertNode = function(node, newNode){
     if(node.key > newNode.key){
       if(node.left == null){
@@ -35,6 +43,33 @@ function BinaryTree(){
     
   }
 
+  //Breadth First Traversal
+  this.breadthFirst = function(root){
+    if(root ==  null){
+      return;
+    }
+
+    let queue = [];
+    queue.push(root);
+
+    while(queue.length != 0){
+
+      let node = queue.shift();
+      console.log(node.key);
+
+      if(node.left != null){
+        queue.push(node.left);
+      }
+
+      if(node.right != null){
+        queue.push(node.right);
+      }
+
+    }
+
+  }
+
+  //Preorder Traversal
   this.preorder = function(node){
     if(node == null){
       return;
@@ -43,7 +78,28 @@ function BinaryTree(){
     console.log(node.key);
     this.preorder(node.left);
     this.preorder(node.right);
+  }
 
+  //Inorder Traversal
+  this.inorder = function(node){
+    if(node == null){
+      return;
+    }
+
+    this.inorder(node.left);
+    console.log(node.key);
+    this.inorder(node.right);
+  }
+
+  //Post-order Traversal
+  this.postorder = function(node){
+    if(node == null){
+      return;
+    }
+
+    this.postorder(node.left);
+    this.postorder(node.right);
+    console.log(node.key);
   }
 }
 
@@ -55,4 +111,15 @@ tree.insert(2);
 tree.insert(5);
 tree.insert(10);
 tree.insert(7);
+tree.insert(3);
+tree.insert(1);
+tree.insert(11);
+tree.insert(8);
+console.log("Preorder");
 tree.preorder(tree.root);
+console.log("Inorder");
+tree.inorder(tree.root);
+console.log("Postorder");
+tree.postorder(tree.root);
+console.log("Breadth First");
+tree.breadthFirst(tree.root);
